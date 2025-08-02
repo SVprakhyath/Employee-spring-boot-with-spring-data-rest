8881e5b6a19cb960008b79e128ef569a2101eedd
Venkata Prakhyath Sundaragiri <venkatasundaragiri@fedex.com>
2025-08-02 20:58:20 +0530

    initial commit

--- spring-boot-rest-crud-employee-with-spring-data-jpa/empdemo/src/main/java/com/employee/springboot/empdemo/dao/EmployeeRepository.java
+++ spring-boot-rest-crud-employee-with-spring-data-jpa/empdemo/src/main/java/com/employee/springboot/empdemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.employee.springboot.empdemo.dao;
 
 import com.employee.springboot.empdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     // No additional methods are needed as JpaRepository provides basic CRUD operations
